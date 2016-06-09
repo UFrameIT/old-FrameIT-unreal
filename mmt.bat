b85dc8050f36e6e523e6f77a40bbf92276dcba0a
Dennis Müller <d.mueller@jacobs-university.de>
2016-06-09 11:16:30 +0200

    V x.y

--- mmt.bat
+++ mmt.bat
@@ -1,1 +1,1 @@
-java -Xmx2048m -cp %~dp0/mmt/frameit-mmt.jar;%~dp0/mmt/mmt.jar info.kwarc.mmt.api.frontend.Run %* --keepalive
+java -Xmx2048m -cp %~dp0/mmt/frameit-mmt.jar;%~dp0/mmt/mmt.jar info.kwarc.mmt.api.frontend.Run %* --keepalive file mmt/startup.msl
